6feb0155558725159d4727752ce163ae6cc3d463
sterago <stefano.rago@blacklane.com>
2018-03-04 17:03:09 +0100

    Using selenium grid to run visual tests

--- test_homepage.py
+++ test_homepage.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from needle.cases import NeedleTestCase
 from needle.driver import NeedleRemote
 from selenium.webdriver import DesiredCapabilities
@@ -6,7 +8,8 @@ from selenium.webdriver import DesiredCapabilities
 class HomepageTests(NeedleTestCase):
     @classmethod
     def get_web_driver(cls):
-        return NeedleRemote(desired_capabilities=DesiredCapabilities.CHROME)
+        return NeedleRemote(desired_capabilities=DesiredCapabilities.CHROME,
+                            command_executor=getenv('SELENIUM_HUB'))
 
     def test_homepage(self):
         self.driver.get('http://www.bbc.co.uk/news/')
