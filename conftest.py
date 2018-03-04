7d7c8fb448ab6b3923b564e269c6107bf3ebeed3
sterago <stefano.rago@blacklane.com>
2018-03-04 18:35:29 +0100

    Debugging runner ip

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ def app():
     app = create_app()
     app.config.update(
         DEBUG=True,
-        SERVER_NAME=getenv('RUNNER_IP')
+        SERVER_NAME=getenv('RUNNER_IP').strip()
     )
 
     return app
